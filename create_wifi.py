9f2236621c25b355106ff7eae74ae8e76d676ba9
Tobias Mueller <muelli@cryptobitch.de>
2015-08-16 21:08:55 +0200

    updated licence to GPLv3+

--- create_wifi.py
+++ create_wifi.py
@@ -2,7 +2,7 @@
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -15,6 +15,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 # Copyright (C) 2011 Red Hat, Inc.
+# Copyright (C) 2015 Tobias Mueller <muelli@cryptobitch.de>
 #
 
 import dbus
--- get_active_connections.py
+++ get_active_connections.py
@@ -3,7 +3,7 @@
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -16,6 +16,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 # Copyright (C) 2010 Red Hat, Inc.
+# Copyright (C) 2015 Tobias Mueller <muelli@cryptobitch.de>
 #
 
 import dbus, sys
